5d183008c6b07eea613e9bd4eb8312d84d03a1b8
Brent Williams <jbwtech@gmail.com>
2023-07-23 20:13:30 -0400

    Mainly unbuffered mode for python + other debugging info

--- sage/docker-entrypoint.sh
+++ sage/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "Running file-watcher ... "
+env | grep DEBUG
 
-python3 file-watcher.py
+if [ "$DEBUG" == "true" ]; then
+    echo -n "Clearing uploads directory ... "
+    sudo rm -rf /files/uploads/*.tex
+    echo "done."
+fi
+
+python3 --version
+
+python3 -u file-watcher.py
