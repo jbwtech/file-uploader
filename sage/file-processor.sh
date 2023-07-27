00a1180b73eccfd573747b555dab626f01f4af67
Brent Williams <jbwtech@gmail.com>
2023-07-27 19:55:32 -0400

    file to handle uploads

---
+++ sage/file-processor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+FILE=$1
+
+sudo chown sage:sage "${UPLOAD_DIR}/${FILE}.tex"
+
+TEMPDIR=$(mktemp -d)
+cd $TEMPDIR
+sudo mv "${UPLOAD_DIR}/${FILE}.tex" ./
+
+pdflatex "${FILE}.tex" > /dev/null 2>&1
+sage "${FILE}.sagetex.sage" > /dev/null 2>&1
+pdflatex "${FILE}.tex" > /dev/null 2>&1
+
+sudo mkdir /files/downloads
+sudo tar zcvf /files/downloads/${FILE}.tgz ${FILE}.* > /dev/null 2>&1
+
+tar ztvf "/files/downloads/${FILE}.tgz"
+
+sudo mv * /files/downloads/
+
+cd ~/
+rm -rf $TEMPDIR
+
+exit 0
